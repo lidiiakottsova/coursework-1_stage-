c8e39b68eaacea14849ad072019e04fda0c9bba8
lidiiakottsova <kottsova.lv@phystech.edu>
2021-05-06 17:01:45 +0300

    final commit

--- mywork.c
+++ mywork.c
@@ -3,82 +3,49 @@
 #include <string.h>
 #include <math.h>
 
-int main(int argc, char * argv[])
+FILE * fout ;
+
+void function_1 (char * frst_str,char * scnd_str ) {
+    if (frst_str[0] == '[') {
+      for (int i = 0; i < 100; i++) scnd_str[i] = 0;
+      for (int i = 1; i < strlen(frst_str); i++) {
+        if (frst_str[i] !=']') {
+          scnd_str[i - 1] = frst_str[i];
+        }
+        else break;
+      }
+    }
+}
+
+void function_2 (char * frst_str,char * scnd_str) {
+  if (frst_str[0] != '[') {
+    fprintf(fout,"%s.", scnd_str);
+    int j = 0;
+    while (frst_str[j] != '=') {
+      j++;
+    }
+    for (int i = 0; i < j; i++) {
+      fprintf(fout,"%c", frst_str[i]);
+    }
+    fprintf(fout,": ");
+    for (int i = j + 1; i < strlen(frst_str); i++) {
+      fprintf(fout,"%c", frst_str[i]);
+    }
+  }
+}
+
+int main (int argc, char * argv[])
 {
-  argc=2;
-  FILE * fin  = fopen(argv[1], "r");   
-  FILE * fout = fopen(argv[2], "w"); 
-  char str_1[100];
-  char str_2[100];
-  char str_3[100];
-  char str_4[100];
-  char str_5[100];
-  char str_6[100];
-  char k[1]={};
-  fgets (str_1, 100, fin);
-  fgets (str_2, 100, fin);
-  fgets (str_3, 100, fin);
-  fgets (str_4, 100, fin);
-  fgets (str_5, 100, fin);
-  fgets (str_6, 100, fin);
-  memmove (str_1, str_1+1, strlen (str_1+1) + 1);
-  memmove (&str_1[strlen(str_1)-2], k, 1);
-  fprintf (fout,"%s.", str_1);
-  int j = 0;
-  while (str_2[j] != '=') {
-    j++;
+  argc = 3;
+  FILE * fin = fopen(argv[1],"r");
+  fout = fopen(argv[2],"w");
+  char frst_str[100];
+  char scnd_str[100] = {};
+  while (fgets(frst_str, 100, fin)) {
+    function_1(frst_str,scnd_str);
+    function_2(frst_str,scnd_str);
   }
-  for (int i = 0; i < j; i++) {
-    fprintf(fout,"%c", str_2[i]);
-  }
-  fprintf(fout,":");
-  for (int i = j + 1; i < strlen(str_2); i++) {
-    fprintf(fout,"%c", str_2[i]);
-  }
-  fprintf (fout,"%s.", str_1);
-  int s = 0;
-  while (str_3[s] != '=') {
-    s++;
-  }
-  for (int i = 0; i < s; i++) {
-    fprintf(fout,"%c", str_3[i]);
-  }
-  fprintf(fout,":");
-  for (int i = s + 1; i < strlen(str_3); i++) {
-    fprintf(fout,"%c", str_3[i]);
-  }
-
-  memmove (str_4, str_4+1, strlen (str_4+1) + 1);
-  memmove (&str_4[strlen(str_4)-2], k, 1);
-  fprintf (fout,"%s.", str_4);
-  int p = 0;
-  while (str_5[p] != '=') {
-    p++;
-  }
-  for (int i = 0; i < p; i++) {
-    fprintf(fout,"%c", str_5[i]);
-  }
-  fprintf(fout,":");
-  for (int i = p + 1; i < strlen(str_5); i++) {
-    fprintf(fout,"%c", str_5[i]);
-  }
-
-  fprintf (fout,"%s.", str_4);
-  int m = 0;
-  while (str_6[m] != '=') {
-    m++;
-  }
-  for (int i = 0; i < m; i++) {
-    fprintf(fout,"%c", str_6[i]);
-  }
-  fprintf(fout,":");
-  for (int i = m + 1; i < strlen(str_6); i++) {
-    fprintf(fout,"%c", str_6[i]);
-  }
-  fclose(fin);               
+  fclose(fin);
   fclose(fout);
   return 0;
 }
-    
-
-
